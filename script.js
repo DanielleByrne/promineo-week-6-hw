3e9d690b1f41689a96b2d5404d673dd50c9b569f
Danielle Byrne <DanielleByrne421@gmail.com>
2021-12-09 18:41:19 -0800

    adding comments for assignment

---
+++ script.js
@@ -0,0 +1,6 @@
+// game of war 
+// deal 26 cards to two players
+// iterate through the turns where each player plays a card
+// the player who played a higher card gets a point
+//display the scores at the end 
+// write a unit test
