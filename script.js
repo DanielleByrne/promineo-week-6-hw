eb03700201511036139721a8dd84dfa64255a72c
Danielle Byrne <DanielleByrne421@gmail.com>
2021-12-11 01:21:38 -0800

    trying to make the deck class, .push isn't working

--- script.js
+++ script.js
@@ -1,18 +1,68 @@
-// game of war 
+// game of war
 // deal 26 cards to two players
 // iterate through the turns where each player plays a card
 // the player who played a higher card gets a point
-//display the scores at the end 
+//display the scores at the end
 // write a unit test
 
 class Card {
-    // suit and value 
+  // suit and value
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
 }
 
-class Deck{
-    // deckOfCards - array of the instances of Card class
+class Deck {
+  // deckOfCards - array of the instances of Card class
+  // method to take a turn and method to shuffle
+  constructor(cardDeck) {
+    this.cardDeck = cardDeck; // will be the array
+  }
+
+  createDeck() {
+    let suits = ["diamond", "club", "heart", "spade"];
+    let value = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "K",
+      "Q",
+      "A",
+    ];
+
+    // calling on deck constructor to make a new card deck with the cards made in the loops
+    let deck = new Deck();
+
+    // nested for loop, will go in order and make a card for each one, will make all spade + value combinations then all club + value combinations ect..
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < value.length; j++) {
+        //calling on Card constructor and making new cards
+        // passing in the suit and value for that iteration
+        let newCard = new Card(suits[i], value[i]);
+        //pushing them to the new array
+        deck.cardDeck.push(newCard);
+      }
+    }
+    console.log(deck);
+  }
 }
+
+
+
+let playerOneDeck = new Deck()
+playerOneDeck.createDeck()
+
 
 class Player {
-    // hand property and a method for a turn 
+  // hand property and a method for a turn
 }
+
+// need playerOne and playerTwo
